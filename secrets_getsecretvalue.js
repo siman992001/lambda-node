59ce000900a453aff7f3bb5baf9892c722bbfb00
Ramon A Lewis <lewis.a.ramon@gmail.com>
2022-05-14 08:54:05 -0400

    Add Secrets

---
+++ secrets_getsecretvalue.js
@@ -0,0 +1,22 @@
+var AWS = require('aws-sdk'),
+  region = 'us-east-1',
+  secret_name = 'python_user',
+  secret
+
+var client = new AWS.SecretsManager({ region: region })
+
+exports.handler = (event, context, callback) => {
+      
+    client.getSecretValue({ SecretId: secret_name }, function (err, data) {
+      if (err) {
+        if (err.code === 'ResourceNotFoundException') {
+          console.log('***************', err.code, '***************')
+        }
+        throw err
+      } else {
+        console.log('***************', data, '***************')
+        secret = data.SecretString
+        console.log('secret is', secret)
+      }
+    })
+};
