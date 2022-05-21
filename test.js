b492e65e3b402924d1abcb85c95bdb5095391678
Ramon A Lewis <lewis.a.ramon@gmail.com>
2022-05-21 04:39:36 -0400

    environment

--- test.js
+++ test.js
@@ -1,4 +1,8 @@
 // var h = require('./secrets_getsecretvalue.js');
 // h.handler()
-var h = require('./http.js')
-h.handler().then((data) => {console.log(data)})
+// var h = require('./http.js')
+// h.handler().then((data) => {console.log(data)})
+var h = require('./env.js')
+h.handler().then((data)=>{
+    console.log(data)
+})
