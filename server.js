72dba645ad508aac06b65ba789101b2286d1cf87
Ramon A Lewis <lewis.a.ramon@gmail.com>
2022-05-25 09:20:54 -0400

    read file

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var http = require('http');
 var url = require('url');
+var fs = require('fs');
 
 http.createServer(function(req, res){
     console.log('url:', req.url);
     var q = url.parse(req.url, true).query;
     console.log('query:', q.name);
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World');
+    fs.readFile('server.js', function(err, data){
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write(data);
+        return res.end();
+    })
 }).listen(4500);
 
 
