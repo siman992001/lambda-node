f6d778e36bfe5ea4adc18e304a4e51fe721ff347
Ramon A Lewis <lewis.a.ramon@gmail.com>
2022-05-25 09:13:27 -0400

    query

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-var http = require('http')
+var http = require('http');
+var url = require('url');
 
 http.createServer(function(req, res){
-    console.log(req.url);
+    console.log('url:', req.url);
+    var q = url.parse(req.url, true).query;
+    console.log('query:', q.name);
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World');
 }).listen(4500);
