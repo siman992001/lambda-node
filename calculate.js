2fec00f16c50b79ae682390ada852ca9d8650687
Ramon A Lewis <lewis.a.ramon@gmail.com>
2022-05-30 10:00:19 -0400

    handler

--- calculate.js
+++ calculate.js
@@ -1,6 +1,9 @@
 var calc = require('./calculator');
 
-console.log('Addition', calc.add(6, 7));
-console.log('Subtract', calc.subtract(5, 2));
-console.log('Multiply', calc.multiply(6, 3));
-console.log('Divide', calc.divide(14, 7));
+
+exports.handler = async function(event, context){
+    console.log('Addition', calc.add(6, 7));
+    console.log('Subtract', calc.subtract(5, 2));
+    console.log('Multiply', calc.multiply(6, 3));
+    console.log('Divide', calc.divide(14, 7));
+}
