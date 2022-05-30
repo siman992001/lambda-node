9ba2bfc51e67044e042bd8c0957dc89576742673
Ramon A Lewis <lewis.a.ramon@gmail.com>
2022-05-30 09:57:20 -0400

    calculate

---
+++ calculate.js
@@ -0,0 +1,6 @@
+var calc = require('./calculator');
+
+console.log('Addition', calc.add(6, 7));
+console.log('Subtract', calc.subtract(5, 2));
+console.log('Multiply', calc.multiply(6, 3));
+console.log('Divide', calc.divide(14, 7));
