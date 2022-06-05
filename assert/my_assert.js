58c9dac6ca1184c277d0d28163da92de1bc74bb8
Ramon A Lewis <lewis.a.ramon@gmail.com>
2022-06-05 09:45:19 -0400

    assert function call

--- assert/my_assert.js
+++ assert/my_assert.js
@@ -1,13 +1,15 @@
 const assert = require('assert')
 
-let x = 4
-let y = 5
-try {
-  assert(x == y)
-} catch(err) {
+exports.handler = function (event, context) {
+  let x = 4
+  let y = 5
+  try {
+    assert(x == y)
+  } catch (err) {
     console.log(err instanceof assert.AssertionError)
     console.log('error', err.message)
-}
+  }
 
-console.log('2 === "2"', 2 === "2")
-console.log('2 == "2"', 2 == "2")
+  console.log('2 === "2"', 2 === '2')
+  console.log('2 == "2"', 2 == '2')
+}
