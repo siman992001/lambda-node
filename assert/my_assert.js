1bf07ddd83b0ddadcecbbc8038482512bed252fe
Ramon A Lewis <lewis.a.ramon@gmail.com>
2022-06-05 09:40:40 -0400

    assert

---
+++ assert/my_assert.js
@@ -0,0 +1,13 @@
+const assert = require('assert')
+
+let x = 4
+let y = 5
+try {
+  assert(x == y)
+} catch(err) {
+    console.log(err instanceof assert.AssertionError)
+    console.log('error', err.message)
+}
+
+console.log('2 === "2"', 2 === "2")
+console.log('2 == "2"', 2 == "2")
